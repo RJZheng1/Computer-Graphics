4b01b2b4163001f48a506cc11ecfb2dd074c03a8
RJZheng1 <stuykid2016@yahoo.com>
2016-02-14 22:10:23 -0500

    Stuff from class.

--- 01/draw.py
+++ 01/draw.py
@@ -1,4 +1,27 @@
 from display import *
 
 def draw_line( screen, x0, y0, x1, y1, color ):
-    pass
+    if(x1 < x0):
+        draw_line(screen, x1, y1, x0, y0, color)
+    else:
+        A = y1 - y0
+        B = x0 - x1
+        d = 2*A + B
+        A *= 2
+        B *= 2
+        if(y1 - y0 > 0):
+            if(y1 - y0 > x1 - x0):
+                pass
+            else:
+                plot(screen, color, x0, y0)
+                while(x0 != x1):
+                    if(d > 0):
+                        y0 += 1
+                        d += B
+                    x0 += 1
+                    d += A
+        else:
+            if(y1 - y0 > x1 - x0):
+                pass
+            else:
+                pass
