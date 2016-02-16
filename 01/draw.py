d62cdfb837f4cf700a1ce34d13b2bb4ad866b5f6
RJZheng1 <stuykid2016@yahoo.com>
2016-02-15 22:52:23 -0500

    Previously put plot() outside of loop. Fixed.

--- 01/draw.py
+++ 01/draw.py
@@ -6,15 +6,15 @@ def draw_line( screen, x0, y0, x1, y1, color ):
     else:
         A = y1 - y0
         B = x0 - x1
-        d = 2*A + B
-        A *= 2
-        B *= 2
+        d = A + A + B
+        A += A
+        B += B
         if(y1 - y0 > 0):
             if(y1 - y0 > x1 - x0):
                 pass
             else:
-                plot(screen, color, x0, y0)
                 while(x0 != x1):
+                    plot(screen, color, x0, y0)
                     if(d > 0):
                         y0 += 1
                         d += B
