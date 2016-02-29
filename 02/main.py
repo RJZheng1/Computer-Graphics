0f0d040c12ba435f193b1d2976ba87581618904e
RJZheng1 <stuykid2016@yahoo.com>
2016-02-29 11:02:21 -0500

    classwork

--- 02/main.py
+++ 02/main.py
@@ -5,22 +5,21 @@ from math import pi
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-matrix = [[],
-          [],
-          [],
-          []]
+x = 100
+y = 100
+r = 20
 
-add_edge(matrix, 0, 0, 0, 100, 0, 0)
-add_edge(matrix, 100, 0, 0, 100, 100, 0)
-add_edge(matrix, 100, 100, 0, 0, 100, 0)
-add_edge(matrix, 0, 100, 0, 0, 0, 0)
+for a in xrange(360):
+    matrix = [[],
+              [],
+              [],
+              []]
+    add_edge(matrix, x+r, y+r, 0, x+r, y+r, 0)
 
-matrix = matrix_mult(make_translate(200, 200, 0), matrix)
-
-matrix = matrix_mult(make_rotZ(pi/12), matrix)
-
-print_matrix(ident(matrix))
-
-draw_lines(matrix, screen, color)
-
+    theta = a*pi/180
+    trans_matrix = matrix_mult(make_translate(x,y,0), make_rotZ(theta))
+    trans_matrix = matrix_mult(trans_matrix, make_translate(-x,-y,0))
+    matrix = matrix_mult(trans_matrix, matrix)
+    draw_lines(matrix, screen, color)
+    
 display(screen)
