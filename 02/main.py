863b8aaf705bb00d30a99e2406ea785487a79e3a
RJZheng1 <stuykid2016@yahoo.com>
2016-02-29 21:52:06 -0500

    Main.py

--- 02/main.py
+++ 02/main.py
@@ -1,25 +1,43 @@
 from display import *
 from draw import *
 from math import pi
+from copy import deepcopy
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-x = 100
-y = 100
-r = 20
+edge = [[],
+     [],
+     [],
+     []]
 
-for a in xrange(360):
+x = XRES/2
+y = YRES/2
+r = 100
+
+for a in xrange(720):
     matrix = [[],
               [],
               [],
               []]
-    add_edge(matrix, x+r, y+r, 0, x+r, y+r, 0)
-
-    theta = a*pi/180
-    trans_matrix = matrix_mult(make_translate(x,y,0), make_rotZ(theta))
-    trans_matrix = matrix_mult(trans_matrix, make_translate(-x,-y,0))
+    add_edge(matrix, x, y+r+r, 0, x, y+r+r, 0)
+    theta = a*pi/360
+    trans_matrix = matrix_mult(
+        matrix_mult(make_translate(x,y+r,0), make_rotZ(theta)),
+        make_translate(-x,-y-r,0))
     matrix = matrix_mult(trans_matrix, matrix)
+    edge[0].extend(matrix[0])
+    edge[1].extend(matrix[1])
+    edge[2].extend(matrix[2])
+    edge[3].extend(matrix[3])
+
+for a in xrange(0,360,60):
+    matrix = deepcopy(edge)
+    theta = a*pi/180
+    trans_matrix = matrix_mult(
+        matrix_mult(make_translate(x,y,0), make_rotZ(theta)),
+        make_translate(-x,-y,0))
+    matrix = matrix_mult(trans_matrix, matrix)    
     draw_lines(matrix, screen, color)
-    
+
 display(screen)
