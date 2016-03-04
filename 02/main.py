284c2f0d7806e98d102c4765a09a83ee78e0a131
RJZheng1 <stuykid2016@yahoo.com>
2016-03-04 10:43:55 -0500

    More circlesgit add .!

--- 02/main.py
+++ 02/main.py
@@ -13,26 +13,27 @@ edge = [[],
 x = XRES/2
 y = YRES/2
 r = 100
+numAng = 720
 
-for a in xrange(720):
-    matrix = [[],
-              [],
-              [],
-              []]
-    add_edge(matrix, x, y+r+r, 0, x, y+r+r, 0)
-    theta = a*pi/360
+for a in xrange(numAng):
+    matrix = [[x],
+              [y+r+r],
+              [0],
+              [1]]
     trans_matrix = matrix_mult(
-        matrix_mult(make_translate(x,y+r,0), make_rotZ(theta)),
+        matrix_mult(make_translate(x,y+r,0), make_rotZ(a*2*pi/numAng)),
         make_translate(-x,-y-r,0))
     matrix = matrix_mult(trans_matrix, matrix)
-    edge[0].extend(matrix[0])
-    edge[1].extend(matrix[1])
-    edge[2].extend(matrix[2])
-    edge[3].extend(matrix[3])
-    
-for a in xrange(6):
+    edge[0].append(matrix[0][0])
+    edge[1].append(matrix[1][0])
+    edge[2].append(matrix[2][0])
+    edge[3].append(matrix[3][0])
+
+numAng = 90
+
+for a in xrange(numAng):
     trans_matrix = matrix_mult(
-        matrix_mult(make_translate(x,y,0), make_rotZ(pi/3)),
+        matrix_mult(make_translate(x,y,0), make_rotZ(2*pi/numAng)),
         make_translate(-x,-y,0))
     edge = matrix_mult(trans_matrix, edge)
     draw_lines(edge, screen, color)
