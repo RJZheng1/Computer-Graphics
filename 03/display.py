096609126040f05b0e94808613a6293f9b265fa8
RJZheng1 <stuykid2016@yahoo.com>
2016-03-20 17:12:53 -0400

    Command line arguments

--- 03/display.py
+++ 03/display.py
@@ -58,6 +58,7 @@ def display( screen ):
     ppm_name = 'pic.ppm'
     save_ppm( screen, ppm_name )
     try:
-        Popen.wait(Popen( ['imdisplay', ppm_name], stdin=PIPE, stdout = PIPE ))
+        Popen.wait(Popen( ['display', ppm_name], stdin=PIPE, stdout = PIPE ))
     except WindowsError:
-        pass
+        Popen.wait(Popen( ['imdisplay', ppm_name], stdin=PIPE, stdout = PIPE ))
+
--- 03/main.py
+++ 03/main.py
@@ -2,10 +2,14 @@ from display import *
 from draw import *
 from parsers import *
 from matrix import *
+import sys
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 
-parse_file( 'script_nocurves', edges, transform, screen, color )
+if(len(sys.argv) != 2):
+    print "Please give only one script file"
+else:
+    parse_file( sys.argv[1], edges, transform, screen, color )
